8d5b95b3f7c00a173dee772728e451c8d1517fdf
frederik750 <frede_rind@hotmail.dk>
2019-09-27 11:24:01 +0200

    Started connection

--- ServerClient/Server/Program.cs
+++ ServerClient/Server/Program.cs
@@ -6,7 +6,8 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Server server = new Server();
+            server.Connect();
         }
     }
 }
---
+++ ServerClient/Server/Server.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Server
+    {
+        public void Connect()
+        {
+            IPAddress localHost = IPAddress.Parse("127.0.0.1");
+            TcpListener server = new TcpListener(localHost, 8000);
+
+            server.Start();
+            Console.WriteLine("Server is listening on port 8000");
+          
+
+        }
+    }
+}
