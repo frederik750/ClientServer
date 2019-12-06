592da5a052798d0b188992bd594bb1014fd9f647
frederik750 <frede_rind@hotmail.dk>
2019-12-06 17:25:37 +0100

    Client work

--- ServerClient/Client/Client.cs
+++ ServerClient/Client/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -8,6 +9,8 @@ namespace Client
 {
     internal static class Client
     {
+        public static bool Connected = false;
+
         public static void RunClient()
         {
             IPAddress iphost = IPAddress.Parse("127.0.0.1");
@@ -20,22 +23,37 @@ namespace Client
                 Console.WriteLine("Looking for connection");
 
                 sender.Connect(remoteEndPoint);
+                Connected = true;
 
                 Console.WriteLine("Connected to -> {0}", sender.RemoteEndPoint);
 
-                byte[] messageSent = Encoding.ASCII.GetBytes("Test Message from client <EOF>");
-                int byteSent = sender.Send(messageSent);
+
+                NetworkStream serverStream = new NetworkStream(sender);
+                StreamReader reader = new StreamReader(serverStream);
 
 
-                byte[] buffer = new byte[1024];
+                String msg = "";
 
-                int byteReceived = sender.Receive(buffer);
-                Console.WriteLine("Message from Server -> {0}",
-                    Encoding.ASCII.GetString(buffer,
-                        0, byteReceived));
+                while (reader.Peek() != -1)
+                {
+                    msg += reader.ReadLine() + "\n";
+                }
 
-                sender.Shutdown(SocketShutdown.Both);
-                sender.Close();
+                Console.WriteLine(msg);
+                /* byte[] messageSent = Encoding.ASCII.GetBytes("Test Message from client <EOF>");
+                 int byteSent = sender.Send(messageSent);
+
+
+                 byte[] buffer = new byte[1024];
+
+                 int byteReceived = sender.Receive(buffer);
+                 Console.WriteLine("Message from Server -> {0}",
+                     Encoding.ASCII.GetString(buffer,
+                         0, byteReceived));
+
+                 sender.Shutdown(SocketShutdown.Both);
+
+                 sender.Close();*/
 
             }
             catch (Exception e)
